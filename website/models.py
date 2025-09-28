1cffb64a8599754cdd4a4624d3114b247bdf24ff
Your Name <your.email@example.com>
2025-09-28 11:44:25 +0000

    update auth

--- website/models.py
+++ website/models.py
@@ -12,6 +12,6 @@ class User(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
+    password = db.Column(db.String(256))
     firstname = db.Column(db.String(150))
     note = db.relationship('Note')
