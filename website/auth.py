c5c8bcefde6edd69beca6821038958de3b1710b6
Your Name <your.email@example.com>
2025-09-28 11:38:17 +0000

    update auth

--- website/auth.py
+++ website/auth.py
@@ -41,7 +41,7 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        user = User.query.filter_by(email = email).fisrt()
+        user = User.query.filter_by(email = email).first()
         if user:
             flash("Email already exists!", category = 'error')
         if len(email) < 4:
